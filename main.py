d30b2cdb944cc7d0a73d63b065dc285deea6db84
wiktoria-kania <155588037+wiktoria-kania@users.noreply.github.com>
2026-06-15 23:50:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,33 @@ from src.parser import parse_host
 from src.result_writer import save_results
 from src.database import insert_result
 from src.database import connect_db
+import os
 
+def check_files():
+    os.makedirs("input", exist_ok=True)
+    os.makedirs("output", exist_ok=True)
+
+    if not os.path.isfile("input/ips.txt"):
+        with open("input/ips.txt", "w", encoding="utf-8"):
+            pass
+
+        print("Utworzono plik input/ips.txt")
+
+    if not os.path.isfile("output/results.txt"):
+            with open("output/results.txt", "w", encoding="utf-8"):
+                pass
+
+            print("Utworzono plik output/results.txt")
+
+    connect_db();
 
 def scan_ips():
+
     ips = load_ips("input/ips.txt")
 
     results = []
 
-    connect_db()
+    connect_db();
 
     for ip in ips:
         raw_data = get_host(ip)
@@ -28,4 +47,3 @@ def scan_ips():
     save_results(results, "output/results.txt")
 
     return results
-
